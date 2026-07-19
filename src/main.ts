b48efe5d0971ac1daa389d47d1d1b4a48baee9a4
Ghostik <ghostik.tech@gmail.com>
2026-07-19 22:54:36 +0300

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { Plugin } from "obsidian";
+
+export default class ObsidianShelf extends Plugin {
+  async onload() {
+    // console.log("Obsidian Shelf loaded");
+  }
+
+  onunload() {
+    // console.log("Obsidian Shelf unloaded");
+  }
+}
